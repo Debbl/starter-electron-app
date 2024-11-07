ebaa25fc76cb9006319f4e1e923cdb56aac03bb6
Brendan Dash <me@aiwan.run>
2024-11-07 17:37:26 +0800

    feat: ignore unused files on production

--- forge.config.ts
+++ forge.config.ts
@@ -4,6 +4,13 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     osxSign: {},
+    ignore: (path) => {
+      if (path === "") return false;
+
+      const keepPaths = ["/dist", "/package.json"];
+
+      return !keepPaths.some((keepPath) => path.startsWith(keepPath));
+    },
   },
   makers: [
     {
