b859beaa7619c74df39a871844e45f3ba2f5d499
Brendan Dash <me@aiwan.run>
2024-11-07 18:14:24 +0800

    feat: add main terser code

--- apps/main/rollup.config.ts
+++ apps/main/rollup.config.ts
@@ -1,10 +1,16 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 import type { InputPluginOption } from "rollup";
 
-const plugins: InputPluginOption = [typescript(), nodeResolve(), commonjs()];
+const plugins: InputPluginOption = [
+  typescript(),
+  nodeResolve(),
+  commonjs(),
+  terser(),
+];
 
 export default defineConfig([
   {
